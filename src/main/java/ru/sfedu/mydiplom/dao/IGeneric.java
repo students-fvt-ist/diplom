0113a2490a7674612af39afbcbe4654554a24681
SeyranChopuryan <chopuryan.sey@gmail.com>
2016-12-20 01:51:12 +0300

    Ночной бред

--- src/main/java/ru/sfedu/mydiplom/dao/IGeneric.java
+++ src/main/java/ru/sfedu/mydiplom/dao/IGeneric.java
@@ -7,17 +7,15 @@ import ru.sfedu.mydiplom.model.dto.ClassType;
 import ru.sfedu.mydiplom.model.dto.GenericDto;
 
 public interface IGeneric {
-
     
     public Result insert(GenericDto object)throws Exception; 
     public Result update(GenericDto object)throws Exception;   
     public Result delete(String arg, String value, ClassType type)throws Exception;  
     public Optional<List<GenericDto>> select(String arg, String value, ClassType type)throws Exception; 
     
-    public Optional<List<GenericDto>> getClientByID(long id) throws Exception;
-    public Optional<List<GenericDto>> getTypeCreditByID(long id) throws Exception;
-    public Optional<List<GenericDto>> getApplicationByID(long id) throws Exception;
-    public Optional<List<GenericDto>> getPaymentByID(long id) throws Exception;
-    public Optional<List<GenericDto>> getDelayByID(long id) throws Exception;
-    
+    public Optional<List<GenericDto>> getObjectByID(long id, ClassType type) throws Exception;
+    public Optional<List<GenericDto>> getDelayByCreditID(long id) throws Exception;
+    public Optional<List<GenericDto>> getPaymentByCreditID(long id) throws Exception;
+    public Optional<List<GenericDto>> getApplicationsByTypeID(long id) throws Exception;
+    public Optional<List<GenericDto>> getApplicatonByClientID(long id) throws Exception;
 }
